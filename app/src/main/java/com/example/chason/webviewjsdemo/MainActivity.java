f7583443bd0cf20903f194c5d5a72f8d850dadb5
范献超 <chason_fan@skytech.cn>
2020-03-02 08:50:52 +0800

    first commit

---
+++ app/src/main/java/com/example/chason/webviewjsdemo/MainActivity.java
@@ -0,0 +1,153 @@
+package com.example.chason.webviewjsdemo;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.JsResult;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView mWebView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.webview);
+
+        WebSettings webSettings = mWebView.getSettings();
+
+        // 设置与Js交互的权限
+        webSettings.setJavaScriptEnabled(true);
+        // 设置允许JS弹窗
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+
+        AndroidtoJsMethod1();
+        AndroidtoJsMethod2();
+
+        // 先载入JS代码
+        // 格式规定为:file:///android_asset/文件名.html
+        mWebView.loadUrl("file:///android_asset/javascript.html");
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 通过Handler发送消息
+                mWebView.post(new Runnable() {
+                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                    @Override
+                    public void run() {
+
+                        // 注意调用的JS方法名要对应上
+                        // Android版本变量
+                        final int version = Build.VERSION.SDK_INT;
+                        // 因为该方法在 Android 4.4 版本才可使用，所以使用时需进行版本判断
+                        if (version < 18) {
+                            // android 调用 JS 方法1
+                            mWebView.loadUrl("javascript:callJS()");
+                        } else {
+                            // android 调用 JS 方法2
+                            mWebView.evaluateJavascript("javascript:callJS()", new ValueCallback<String>() {
+                                @Override
+                                public void onReceiveValue(String value) {
+                                    //此处为 js 返回的结果
+                                }
+                            });
+                        }
+
+                    }
+                });
+            }
+        });
+
+        // 由于设置了弹窗检验调用结果,所以需要支持js对话框
+        // webview只是载体，内容的渲染需要使用webviewChromClient类去实现
+        // 通过设置WebChromeClient对象处理JavaScript的对话框
+        //设置响应js 的Alert()函数
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
+                AlertDialog.Builder b = new AlertDialog.Builder(MainActivity.this);
+                b.setTitle("Alert 2020");
+                b.setMessage(message);
+                b.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        result.confirm();
+                    }
+                });
+                b.setCancelable(false);
+                b.create().show();
+                return true;
+            }
+
+        });
+    }
+
+    private void AndroidtoJsMethod1() {
+        // 通过addJavascriptInterface()将Java对象映射到JS对象
+        //参数1：Javascript对象名
+        //参数2：Java对象名
+        mWebView.addJavascriptInterface(new AndroidtoJs(), "test");//AndroidtoJS类对象映射到js的test对象
+    }
+
+    private void AndroidtoJsMethod2() {
+// 复写WebViewClient类的shouldOverrideUrlLoading方法
+        mWebView.setWebViewClient(new WebViewClient() {
+                                      @Override
+                                      public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                                          // 步骤2：根据协议的参数，判断是否是所需要的url
+                                          // 一般根据scheme（协议格式） & authority（协议名）判断（前两个参数）
+                                          //假定传入进来的 url = "js://webview?arg1=111&arg2=222"（同时也是约定好的需要拦截的）
+
+                                          Uri uri = Uri.parse(url);
+                                          // 如果url的协议 = 预先约定的 js 协议
+                                          // 就解析往下解析参数
+                                          if (uri.getScheme().equals("js")) {
+
+                                              // 如果 authority  = 预先约定协议里的 webview，即代表都符合约定的协议
+                                              // 所以拦截url,下面JS开始调用Android需要的方法
+                                              if (uri.getAuthority().equals("webview")) {
+
+                                                  //  步骤3：
+                                                  // 执行JS所需要调用的逻辑
+                                                  // 可以在协议上带有参数并传递到Android上
+                                                  HashMap<String, String> params = new HashMap<>();
+//                                                  Set<String> collection = uri.getQueryParameterNames();
+                                                  String query = uri.getQuery();
+                                                  System.out.println("JS调用了Android的方法" + query);
+
+                                                  String result = "666";
+                                                  mWebView.loadUrl("javascript:returnResult(" + result + ")");
+
+                                              }
+
+                                              return true;
+                                          }
+                                          return super.shouldOverrideUrlLoading(view, url);
+                                      }
+                                  }
+        );
+    }
+
+}
+
